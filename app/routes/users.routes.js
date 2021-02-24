0ec1cf5dc04d260e119656d155c055478c7d291e
omar bdeer <omar bdeer@DESKTOP-S5OQK6B>
2021-02-24 19:55:19 +0200

    upload server nodejs

---
+++ app/routes/users.routes.js
@@ -0,0 +1,12 @@
+module.exports = app => {
+  const users = require("../controllers/users.controller.js");
+
+  var router = require("express").Router();
+
+  router.post("/user", users.createUser);
+  router.get("/users", users.getUsers);
+  router.put("/status", users.updateStatus);
+  router.delete("/user", users.deleteUser);
+
+  app.use('', router);
+};
